87a202b49545c7eed980561e5353636feda9abe2
Anay <anaydesh1234@gmail.com>
2022-11-26 01:55:57 +0530

    commands, functions, pannels added
    1.  Remove tag command
    2.  Remove tag function
    3.  Panels added everywhere
    4.  Tag command

--- app/modules/Exceptions.py
+++ app/modules/Exceptions.py
@@ -1,20 +1,21 @@
 from rich import print
+from rich.panel import Panel
 
 class WordNeverSearchedException(Exception):
     """raised when a word is never searched but user attempts to perform some operation on it."""
     word=None
     def __init__(self, word):
         self.word=word
-        print(f"The word [bold red]{self.word}[/bold red] was never tracked before. Add some words in your list using 'define' command first.")
+        print(Panel(f"The word [bold red]{self.word}[/bold red] was never tracked before. Add some words in your list using 'define' command first. 🔎"))
         
         
 class AudioUnavailableException(Exception):
     """raised when the audio is not available for the word."""
     def __init__(self):
-        print("[bold red]Audio Unavailable[/bold red]")
+        print(Panel("[bold red]Audio Unavailable[/bold red] ❌"))
         
 
 class NoDataFoundException(Exception):
     """raised when the user attempts to export data but there is no data to export."""
     def __init__(self):
-        print("[bold red]No words to export. Add some words in your list using 'define' command first.[/bold red]")
+        print(Panel("[bold red]No words to export ❌. Add some words in your list using 'define' command fisear➕. [/bold red]"))
