38af3f4f265faf5eac5cde277413f3c3032533d8
HighnessAtharva <HighnessAtharva@gmail.com>
2022-11-29 15:00:20 +0530

    empty commit

--- tests/test_VocabularyBuilder.py
+++ tests/test_VocabularyBuilder.py
@@ -9,8 +9,8 @@ import shutil
 from pathlib import Path
 import sqlite3
 from typer.testing import CliRunner
-from VocabularyBuilder import app
- 
+from VocabularyCLI import app
+
 runner=CliRunner()
 
 
@@ -19,10 +19,10 @@ def setup_module():
     Will run before any test.
     Setup any state specific to the execution.
     """
-        
+
     if not os.path.exists("VocabularyBuilder.db"):
-        return 
-    
+        return
+
     # move the app database to the parent directory
     app_DB_path=os.path.join(os.getcwd(), "VocabularyBuilder.db")
     parent_dir = Path(os.getcwd()).parents[0]
@@ -46,17 +46,17 @@ def setup_module():
 def teardown_module():
     """
     Will run after all tests.
-    Teardown any state that was previously setup with a setup_module method. 
+    Teardown any state that was previously setup with a setup_module method.
     """
-    
-    # close the connection 
+
+    # close the connection
     conn = sqlite3.connect('./VocabularyBuilder.db')
     conn.close()
-    
+
     # delete the test database
     if os.path.exists("VocabularyBuilder.db"):
         os.remove("VocabularyBuilder.db")
-        
+
     # move the app database back to the app folder
     app_DB_path=os.path.join(Path(os.getcwd()).parents[0], "VocabularyBuilder.db")
     current_dir =  os.getcwd()
@@ -77,32 +77,32 @@ class TestDefine:
         result = runner.invoke(app, ["define", "hello"])
         assert result.exit_code == 0
         assert "5. " in result.stdout
-            
+
     def test_define_fake_word(self):
         result= runner.invoke(app, ["define", "fakewordhaha"])
         assert result.exit_code == 0
-        assert """is not a valid word""" in result.stdout       
+        assert """is not a valid word""" in result.stdout
 
-    def test_define_short(self):  
+    def test_define_short(self):
         result = runner.invoke(app, ["define", "hello", "--short"])
         assert result.exit_code == 0
         assert """"Hello!" or an equivalent greeting""" in result.stdout
-        
+
     def test_define_short_fake_word(self):
         result= runner.invoke(app, ["define", "fakewordhaha", "--short"])
         assert result.exit_code == 0
         assert """is not a valid word""" in result.stdout
-        
+
     def test_define_pronounce(self):
         result= runner.invoke(app, ["define", "hello", "--pronounce"])
         assert result.exit_code == 0
         assert """Audio played""" in result.stdout
-        
+
     def test_define_pronounce_fake_word(self):
         result= runner.invoke(app, ["define", "fakewordhaha", "--pronounce"])
         assert result.exit_code == 0
         assert """is not a valid word""" in result.stdout
-        
+
     def test_define_pronounce_unavailable(self):
         result= runner.invoke(app, ["define", "extraordinary", "--pronounce"])
         assert result.exit_code == 0
@@ -119,8 +119,8 @@ class TestDefine:
         assert result.exit_code == 0
         assert """is not a valid word""" in result.stdout #error msg for fake word
         assert """To direct a radar beam toward""" in result.stdout #substr from def of second word
-        
-        
+
+
 # test cases for favorite and unfavorite commands
 class TestFavorite:
     def test_favorite(self):
@@ -129,11 +129,11 @@ class TestFavorite:
 
         # reset favorite value if it was already favorite (edge case)
         runner.invoke(app, ["unfavorite", "hello"])
-                
+
         result = runner.invoke(app, ["favorite", "hello"])
         assert result.exit_code == 0
         assert "has been set as favorite" in result.stdout
-        
+
     def test_favorite_multiple_words(self):
         # adding words to DB using define if it doesn't already exist
         runner.invoke(app, ["define", "hello", "world"])
@@ -142,7 +142,7 @@ class TestFavorite:
         assert result.exit_code == 0
         assert "hello has been set as favorite" in result.stdout
         assert "world has been set as favorite" in result.stdout
-        
+
     def test_unfavorite_multiple_words(self):
         runner.invoke(app, ["define", "calm", "morning"] )
         runner.invoke(app, ["favorite", "calm", "morning"])
@@ -150,22 +150,22 @@ class TestFavorite:
         assert result.exit_code == 0
         assert "calm has been removed from favorite" in result.stdout
         assert "morning has been removed from favorite" in result.stdout
-        
+
     def test_favorite_fake_word(self):
         result = runner.invoke(app, ["favorite", "fakewordhaha"])
         assert result.exit_code == 0
         assert "was never tracked before. Add some words" in result.stdout
-        
+
     def test_favorite_already_favorite(self):
         result = runner.invoke(app, ["favorite", "hello"])
         assert result.exit_code == 0
         assert "is already marked as favorite" in result.stdout
-        
+
     def test_unfavorite(self):
         result = runner.invoke(app, ["unfavorite", "hello"])
         assert result.exit_code == 0
         assert "has been removed from favorite" in result.stdout
-           
+
     def test_unfavorite_not_favorite(self):
         result = runner.invoke(app, ["unfavorite", "hello"])
         assert result.exit_code == 0
@@ -177,7 +177,7 @@ class TestFavorite:
         assert result.exit_code == 0
         assert "was never tracked before. Add some words" in result.stdout
 
-    
+
 # test cases for learn and unlearn commands
 class TestLearn:
     def test_learn(self):
@@ -186,7 +186,7 @@ class TestLearn:
         result = runner.invoke(app, ["learn", "hello"])
         assert result.exit_code == 0
         assert "has been set as learning. Keep revising!" in result.stdout
-        
+
     def test_learn_multiple_words(self):
         runner.invoke(app, ["define", "hello", "world"])
         runner.invoke(app, ["unlearn", "hello", "world"])
@@ -194,7 +194,7 @@ class TestLearn:
         assert result.exit_code == 0
         assert "hello has been set as learning" in result.stdout
         assert "world has been set as learning" in result.stdout
-    
+
     def test_unlearn_multiple_words(self):
         runner.invoke(app, ["define", "white", "black"])
         runner.invoke(app, ["learn", "white", "black"])
@@ -207,17 +207,17 @@ class TestLearn:
         result = runner.invoke(app, ["learn", "fakewordhaha"])
         assert result.exit_code == 0
         assert "was never tracked before. Add some words" in result.stdout
-        
+
     def test_learn_already_learn(self):
         result = runner.invoke(app, ["learn", "hello"])
         assert result.exit_code == 0
         assert "is already marked as learning" in result.stdout
-        
+
     def test_unlearn(self):
         result = runner.invoke(app, ["unlearn", "hello"])
         assert result.exit_code == 0
         assert "has been removed from learning" in result.stdout
-           
+
     def test_unlearn_not_learn(self):
         result = runner.invoke(app, ["unlearn", "hello"])
         assert result.exit_code == 0
@@ -231,14 +231,14 @@ class TestLearn:
 
 
 # test cases for master and unmaster commands
-class TestMaster:    
-    def test_master(self):    
+class TestMaster:
+    def test_master(self):
         # adding this word to learning list programatically
         runner.invoke(app, ["define", "hello"])
-        
+
         # reset master value if it was already master (edge case)
         runner.invoke(app, ["unmaster", "hello"])
-        
+
         result = runner.invoke(app, ["master", "hello"])
         assert result.exit_code == 0
         assert "has been set as mastered. Good work!" in result.stdout
@@ -246,7 +246,7 @@ class TestMaster:
         result=runner.invoke(app, ["unlearn", "hello"])
         assert result.exit_code == 0
         assert "was never learning" in result.stdout
-        
+
     def test_master_multiple_words(self):
         runner.invoke(app, ["define", "hello", "world"])
         runner.invoke(app, ["learn", "hello", "world"])
@@ -262,42 +262,42 @@ class TestMaster:
         assert result.exit_code == 0
         assert "hello has been set as learning" in result.stdout
         assert "world has been set as learning" in result.stdout
-        
+
     def test_master_fake_word(self):
         result = runner.invoke(app, ["master", "fakewordhaha"])
         assert result.exit_code == 0
         assert "was never tracked before. Add some words" in result.stdout
-        
+
     def test_master_already_master(self):
         runner.invoke(app, ["define", "hello"])
         runner.invoke(app, ["master", "hello"])
         result = runner.invoke(app, ["master", "hello"])
         assert result.exit_code == 0
         assert "is already marked as mastered" in result.stdout
-        
+
     def test_unmaster(self):
         result = runner.invoke(app, ["unmaster", "hello"])
         assert result.exit_code == 0
         assert "has been set as learning" in result.stdout
-           
+
     def test_unmaster_not_master(self):
         result = runner.invoke(app, ["unmaster", "hello"])
         assert result.exit_code == 0
         assert "was never mastered" in result.stdout
-        
+
     def test_unmaster_fake_word(self):
         result = runner.invoke(app, ["unmaster", "fakewordhaha"])
         assert result.exit_code == 0
         assert "was never tracked before. Add some words" in result.stdout
 
-class TestTag:    
+class TestTag:
     def test_tag(self):  # sourcery skip: class-extract-method
         runner.invoke(app, ["delete", "hello"])
         runner.invoke(app, ["define", "hello"])
         result = runner.invoke(app, ["tag", "hello","--name", "testtag"])
         assert result.exit_code == 0
         assert "has been tagged as" in result.stdout
-    
+
     def test_tag_multiple_words(self):
         runner.invoke(app, ["define", "hello", "world"])
         runner.invoke(app, ["untag", "hello", "world"])
@@ -305,8 +305,8 @@ class TestTag:
         assert result.exit_code == 0
         assert "hello has been tagged as" in result.stdout
         assert "world has been tagged as" in result.stdout
-        
-    
+
+
     def test_untag_multiple_words(self):
         runner.invoke(app, ["define", "hello", "world"])
         runner.invoke(app, ["tag", "hello", "world", "--name", "testtag"])
@@ -314,19 +314,19 @@ class TestTag:
         assert result.exit_code == 0
         assert "Tags deleted for the word hello" in result.stdout
         assert "Tags deleted for the word world" in result.stdout
-    
+
     def test_tag_already_exists(self):
         runner.invoke(app, ["tag", "hello", "--name", "testtag"])
         result = runner.invoke(app, ["tag", "hello", "--name", "testtag2"])
         assert result.exit_code == 0
         assert "tag has been changed to" in result.stdout
-    
+
     def test_tag_fake_word(self):
         result=runner.invoke(app, ["tag", "fakeworkhaha", "--name", "testtag"])
-        assert result.exit_code == 0 
+        assert result.exit_code == 0
         assert "is not a valid word" in result.stdout
-        
-    def test_untag(self):        
+
+    def test_untag(self):
         runner.invoke(app, ["define", "hello"])
         runner.invoke(app, ["delete", "hello"])
         runner.invoke(app, ["define", "hello"])
@@ -347,74 +347,74 @@ class TestTag:
         result = runner.invoke(app, ["untag", "fakewordhaha"])
         assert result.exit_code == 1 # exception raised
         assert "was never tracked before." in result.stdout
-        
+
 
 class TestDelete:
     def test_delete(self):
         runner.invoke(app, ["define", "hello"])
         result = runner.invoke(app, ["delete", "hello"])
-        assert result.exit_code == 0 
+        assert result.exit_code == 0
         assert "hello deleted from your lists." in result.stdout
-    
+
     def test_delete_multiple_words(self):
         runner.invoke(app, ["define", "hello", "world"])
         result = runner.invoke(app, ["delete", "hello", "world"])
-        assert result.exit_code == 0 
+        assert result.exit_code == 0
         assert "hello deleted from your lists." in result.stdout
-    
-    
+
+
     def test_delete_unadded_word(self):
         runner.invoke(app, ["define", "fakewordhaha"])
         result = runner.invoke(app, ["delete", "fakewordhaha"])
-        assert result.exit_code == 0 
+        assert result.exit_code == 0
         assert "was never tracked before" in result.stdout
-        
-    
+
+
 class TestClear:
     def test_clear_all(self):
         runner.invoke(app, ["clear", "--all"])
         runner.invoke(app, ["define", "hello", "world", "smash", "--short"])
         result = runner.invoke(app, ["clear", "--all"])
-        assert result.exit_code == 0 
+        assert result.exit_code == 0
         assert "All words[3] deleted" in result.stdout
-    
+
     def test_clear_learning(self):
         runner.invoke(app, ["define", "hello", "world", "smash", "--short"])
         runner.invoke(app, ["learn", "hello", "world"])
         result = runner.invoke(app, ["clear", "--learning"])
-        assert result.exit_code == 0 
+        assert result.exit_code == 0
         assert "All learning words[2] deleted" in result.stdout
-    
+
     def test_clear_mastered(self):
         runner.invoke(app, ["define", "hello", "world", "smash", "--short"])
         runner.invoke(app, ["master", "hello", "world"])
         result = runner.invoke(app, ["clear", "--mastered"])
-        assert result.exit_code == 0 
+        assert result.exit_code == 0
         assert "All mastered words[2] deleted" in result.stdout
-    
+
     def test_clear_favorite(self):
         runner.invoke(app, ["define", "hello", "world", "smash", "--short"])
         runner.invoke(app, ["favorite", "hello", "world"])
         result = runner.invoke(app, ["clear", "--favorite"])
-        assert result.exit_code == 0 
+        assert result.exit_code == 0
         assert "All favorite words[2] deleted" in result.stdout
-    
+
     def test_clear_tag(self):
         runner.invoke(app, ["define", "hello", "world", "smash", "--short"])
         runner.invoke(app, ["tag", "hello", "world", "--name", "testtag"])
         result = runner.invoke(app, ["clear", "--tag", "testtag"])
-        assert result.exit_code == 0 
+        assert result.exit_code == 0
         assert "All words[2] with tag testtag deleted" in result.stdout
-    
+
     def test_clear_with_empty_db(self):
         runner.invoke(app, ["clear", "--all"])
         result = runner.invoke(app, ["clear", "--all"])
-        assert result.exit_code == 0 
+        assert result.exit_code == 0
         assert "Nothing to delete." in result.stdout
-        
-    
-    
- 
+
+
+
+
 class TestList:
     pass
 
@@ -422,22 +422,22 @@ class TestImportExport:
     def test_pdf_export(self):
         runner.invoke(app, ["define", "hello"])
         result = runner.invoke(app, ["export", "--pdf"])
-        assert result.exit_code == 0 
+        assert result.exit_code == 0
         assert "WORDS TO PDF" in result.stdout
         # delete the created file
         test = os.listdir(os.getcwd())
         for item in test:
             if item.endswith(".pdf"):
                 os.remove(os.path.join(os.getcwd(), item))
-          
+
     def test_csv_import_with_duplicates(self):
         runner.invoke(app, ["define", "hello"])
         runner.invoke(app, ["export"])
         result = runner.invoke(app, ["import"])
         assert result.exit_code == 0
         assert "WITH THE SAME TIMESTAMP" in result.stdout
-        
-    # WARNING: Running this test will delete all the words in the database. Might want to comment it out.     
+
+    # WARNING: Running this test will delete all the words in the database. Might want to comment it out.
     def test_csv_import_without_duplicates(self):
         runner.invoke(app, ["define", "math","echo", "chamber"])
         runner.invoke(app, ["export"])
@@ -445,14 +445,14 @@ class TestImportExport:
         result = runner.invoke(app, ["import"])
         assert result.exit_code == 0
         assert "IMPORTED" in result.stdout
-        
+
     def test_csv_import_no_file(self):
         # programatically delete the csv file (if exists)
         test = os.listdir(os.getcwd())
         for item in test:
             if item.endswith(".csv"):
                 os.remove(os.path.join(os.getcwd(), item))
-        
+
         # try to import non-existent file
         result=runner.invoke(app, ["import"])
         assert result.exit_code == 0
