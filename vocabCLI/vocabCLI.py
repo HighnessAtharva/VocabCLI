d48fc15f8f48dce54e1b86b2fdebefc24ddb99cb
HighnessAtharva <HighnessAtharva@gmail.com>
2023-01-07 15:19:44 +0530

    cleanup for package building
    Made some files related to packaging and deleted older files to clean up and organize the folder.

--- vocabCLI/vocabCLI.py
+++ vocabCLI/vocabCLI.py
@@ -938,24 +938,19 @@ def flashcard():
     pass
 
 
-  
 
-if __name__ == "__main__":
+# check if Vocabulary.db exists, if not create it
+if not os.path.exists("VocabularyBuilder.db"):
     
-    # check if Vocabulary.db exists, if not create it
-    if not os.path.exists("VocabularyBuilder.db"):
-        
-        # initialize the database with the tables if not already existing
-        initializeDB()
+    # initialize the database with the tables if not already existing
+    initializeDB()
+    # uncomment this to easily delete all words from collections table during testing
+    delete_collection_from_DB()
+    clean_collection_csv_data()
+    # add all the collection words to the database if not already existing
+    insert_collection_to_DB()
 
-        # uncomment this to easily delete all words from collections table during testing
-        delete_collection_from_DB()
-        clean_collection_csv_data()
-
-        # add all the collection words to the database if not already existing
-        insert_collection_to_DB()
-   
-    app()
+app()
 
     
 
